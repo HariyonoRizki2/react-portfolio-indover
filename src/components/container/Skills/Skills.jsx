ba7bcffeca0e288d1ca3f5bc186e0d0c5ad85a4a
HARIYONO RIZKI <nabilaputridwianjani526@gmail.com>
2022-12-28 20:20:39 +0700

    Update Skills.jsx

--- src/components/container/Skills/Skills.jsx
+++ src/components/container/Skills/Skills.jsx
@@ -13,7 +13,7 @@ const Skills = () => {
         whileInView={{y: [-50, 0], opacity: 1}}  
         className="title"
       >
-        <span>What I Expert?</span>
+        <span>What Am I Good At?</span>
         <h1>Skills And Experience</h1>
       </motion.div>
       <motion.div
