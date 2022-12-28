d78e10baa95a7746b153239a25a34d7228f21519
HARIYONO RIZKI <nabilaputridwianjani526@gmail.com>
2022-12-28 19:54:49 +0700

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,5 @@
 import { FaHtml5, FaCss3Alt, FaNodeJs, FaUser, FaMapMarkerAlt, FaInstagram, FaLinkedin, FaTwitter, FaWhatsapp } from "react-icons/fa";
 import { SiTypescript, SiSketchup, SiAdobephotoshop, SiJavascript, SiGmail, SiPhp, SiMysql } from "react-icons/si";
-import work1 from "./assets/work1.png";
-import work2 from "./assets/work2.png";
-import work3 from "./assets/work3.png";
-import work4 from "./assets/work4.png";
-import work5 from "./assets/work5.png";
-import work6 from "./assets/work6.png";
 import dev1 from "./assets/dev1.png";
 
 export const navLinks = ["home", "about", "skills", "portfolio", "contact"]
